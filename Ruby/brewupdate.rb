dbf09b96488388a2fbe592a1e6bedc5f1505971b
Chad Woodhead <chadwoodhead@gmail.com>
2020-03-09 21:30:41 -0500

    update formula with 1.0.1 with dependency on terminal-notifier

--- Ruby/brewupdate.rb
+++ Ruby/brewupdate.rb
@@ -1,10 +1,11 @@
 class Brewupdate < Formula
   desc "Updater for homebrew"
   homepage "https://github.com/chadwoodhead/homebrew-brewupdate"
-  url "https://github.com/chadwoodhead/homebrew-brewupdate/blob/master/archive/homebrew-brewupdate_1.0.tar.gz"
-  version "1.0.0"
-  sha256 "3db1f777dcec30a0e720207daf6ebf7e3e6e215880bccdf4042f7df70cdc1d05"
+  url "https://github.com/chadwoodhead/homebrew-brewupdate/blob/master/archive/homebrew-brewupdate_1.0.1.tar.gz"
+  version "1.0.1"
+  sha256 "a5b6db3993492c755c839e027d86da40a11c97cc59efa6f0525e0cbbfe7351e3"
 
+  depends_on "terminal-notifier"
 
   def install
 	bin.install "brewupdate"
