96902241e8e98c644915d245013b8bdedd77e1f8
Chad Woodhead <chadwoodhead@gmail.com>
2020-03-09 21:13:46 -0500

    changed version from 1.0 to 1.0.0

--- Ruby/brewupdate.rb
+++ Ruby/brewupdate.rb
@@ -2,7 +2,7 @@ class Brewupdate < Formula
   desc "Updater for homebrew"
   homepage "https://github.com/chadwoodhead/homebrew-brewupdate"
   url "https://github.com/chadwoodhead/homebrew-brewupdate/blob/master/archive/homebrew-brewupdate_1.0.tar.gz"
-  version "1.0"
+  version "1.0.0"
   sha256 "3db1f777dcec30a0e720207daf6ebf7e3e6e215880bccdf4042f7df70cdc1d05"
 
 
