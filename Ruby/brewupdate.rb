28bf98382bb00beedd17455e42fd124f9c64bf87
Chad Woodhead <chadwoodhead@gmail.com>
2020-03-10 16:16:49 -0500

    update brewupdate.rb to 1.0.3

--- Ruby/brewupdate.rb
+++ Ruby/brewupdate.rb
@@ -1,9 +1,9 @@
 class Brewupdate < Formula
   desc "Updater for homebrew"
   homepage "https://github.com/chadwoodhead/homebrew-brewupdate"
-  url "https://github.com/chadwoodhead/homebrew-brewupdate/blob/master/archive/homebrew-brewupdate_1.0.2.tar.gz"
-  version "1.0.2"
-  sha256 "e59c0eb2fe03641901dba53a59a264056c73c66f3c7831b3ee41c21641fae24f"
+  url "https://github.com/chadwoodhead/homebrew-brewupdate/blob/master/archive/homebrew-brewupdate_1.0.3.tar"
+  version "1.0.3"
+  sha256 "de3e28f4f585b959fa66fc75444987814b2a338964721635d654624cf3cabc60"
 
   depends_on "terminal-notifier"
 
